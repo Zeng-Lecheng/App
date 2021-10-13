7e81f3e798be0fb6c69180cda8dc5717549cf14a
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2021-10-13 14:55:17 -0400

    nav basics

--- App.js
+++ App.js
@@ -1,64 +1,40 @@
-import React, {useState} from 'react';
-import { Image, ScrollView, Text, StyleSheet, View, TextInput, Pressable } from 'react-native';
+import React from 'react';
+import {Text, Button, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const logo = {
-  uri: 'https://reactnative.dev/img/tiny_logo.png',
-};
+const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-    outer: {
-        padding: 45,
-    },
-    input: {
-      borderWidth: 1,
-      borderColor: "black",
-      height: 20,
-      fontSize: 16
-    },
-    output: {
-      padding: 10,
-      fontSize: 16,
-      fontFamily: "Times New Roman"
-    },
-    button: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 12,
-      paddingHorizontal: 32,
-      borderRadius: 8,
-      elevation: 3,
-      height: 40,
-      width: 200,
-      backgroundColor: '#CA6510',
-    },
-})
-
-const App = () => {
-  // const [number, setNumber] = useState(0);
-  const [text, setText] = useState("default");
-  const [button, setButton] = useState();
-  var uuu = "uuu"
+const MyStack = () => {
   return (
-  <View style={styles.outer}>
-    <TextInput 
-      style={styles.input} 
-      defaultValue="0"
-      onChangeText={(t) => setText(t)}
-      defaultValue={"ddd"}
-    />
-    <Pressable 
-      style={styles.button}
-      onPress={() => setButton(text)}
-    >
-      <Text style={{color: "#fff"}}>
-        {"Test"}
-      </Text>
-    </Pressable>
-    <Text>
-      {button}
-    </Text>
-  </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-export default App;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={{alignItems: "flex-start"}}>
+    <Button 
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', { name: 'Jane' })
+      }
+    />
+    </View>
+  );
+};
+
+const ProfileScreen = ({ navigation, route }) => {
+  return (<Text>This is {route.params.name}'s profile</Text>);
+};
+
+export default MyStack;
