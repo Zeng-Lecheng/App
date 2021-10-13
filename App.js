2bb4bb1ff5a0c881256d270991ca9dcca61a6fdf
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2021-10-13 15:52:38 -0400

    move components apart

--- App.js
+++ App.js
@@ -1,40 +1,24 @@
 import React from 'react';
-import {Text, Button, View} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './components/Home'
+import ProfileScreen from './components/Sub1'
 
 const Stack = createNativeStackNavigator();
 
 const MyStack = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View style={{alignItems: "flex-start"}}>
-    <Button 
-      title="Go to Jane's profile"
-      onPress={() =>
-        navigation.navigate('Profile', { name: 'Jane' })
-      }
-    />
-    </View>
-  );
-};
-
-const ProfileScreen = ({ navigation, route }) => {
-  return (<Text>This is {route.params.name}'s profile</Text>);
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{ title: 'Welcome' }}
+                />
+                <Stack.Screen name="Profile" component={ProfileScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
 export default MyStack;
---
+++ components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, Button, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const HomeScreen = ({ navigation }) => {
+    return (
+        <View style={{ alignItems: "flex-start" }}>
+            
+        </View>
+    );
+};
+
+export default HomeScreen;
