78baad6e2a354170cab60cfb08adc3c4099414d4
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2021-11-18 18:44:27 -0500

    init

--- App.js
+++ App.js
@@ -1,112 +1,126 @@
 import React, { useState } from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, TextInput } from 'react-native';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, Button } from 'react-native';
 import axios from 'axios';
 
 const App = () => {
-  const [inputID, setInputID] = useState("");
-  const [show, setShow] = useState(false);
-  const [repoData, setRepoData] = useState([{"name": "NONE"}]);
+    const [BBoardNames, setBBoardNames] = useState([]);
+    const [initializing, setInitializing] = useState(true)
+    const [BBoardContent, setBBoardContent] = useState()
+    const [selected, setSelected] = useState()
 
-  const getGithub = (userID) => {
-    axios.get(`https://api.github.com/users/${userID}/repos`)
-      .then((response) => {
-        setRepoData(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  };
-
-  const renderItem = ({ item }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{item.name}</Text>
-    </View>
-  );
-
-  const toggleShow = () => {
-    if (show) {
-      return (
-        <View style={{ padding: 5 }}>
-          <Text
-            style={styles.toggle}
-            onPress={() => {
-              setShow(false);
-              setRepoData([{"name": "NONE"}]);
-            }}
-          >
-            hide repositories
-          </Text>
-        </View>
-      );
-    } else {
-      return (
-        <View style={{ padding: 5 }}>
-          <Text
-            style={styles.toggle}
-            onPress={() => {
-              setShow(true);
-              getGithub(inputID);
-            }}
-          >
-            show repositories
-          </Text>
-        </View>
-      );
+    const getBBoardName = () => {
+        axios.get("https://glacial-hamlet-05511.herokuapp.com/bboardNames")
+            .then((response) => {
+                setBBoardNames(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     };
-  };
 
-  return (
-    <SafeAreaView style={[styles.container]}>
-      <View style={{ height: 105, backgroundColor: "black", alignItems: "center", justifyContent: "center" }}>
-        <Text style={{ color: "red", fontSize: 42 }}>Github Viewer</Text>
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        <Text style={styles.input}> github Id: </Text>
-        <TextInput
-          onChangeText={(t) => { setInputID(t); }}
-          value={inputID}
-          style={styles.input}
-          placeholder={"userid"}
-        />
-      </View>
-      {toggleShow()}
-      <FlatList
-        data={repoData}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.name}
-        alignItems="flex-start"
-      />
-      <Text style={styles.debug}>DEBUGGING</Text>
-      <Text style={styles.debug}>userId: {inputID}</Text>
-      <Text style={styles.debug}>showReps: {`${show}`}</Text>
-      <Text style={styles.debug}> repos.length: {repoData.length}</Text>
-    </SafeAreaView>
-  );
+    const getBBoardContent = (name) => {
+        axios.post("https://glacial-hamlet-05511.herokuapp.com/posts", { bboard: name })
+            .then((response) => {
+                setBBoardContent(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    const renderBBoardNames = ({ item }) => (
+        <View style={styles.bboardNameContainer}>
+            <Text
+                style={styles.BBoardNameText}
+                onPress={() => {
+                    setSelected(item)
+                    getBBoardContent(item)
+                }}
+            >
+                {item === "" ? " " : item}
+            </Text>
+        </View>
+    );
+
+    const renderBBoardContent = ({ item }) => (
+        <View sytle={{
+            backgroundColor: "gray",
+            padding: 20,
+            marginHorizontal: 8,
+            marginVertical: 0
+        }}>
+            <Text style={{ fontSize: 32 }}>{item.title}</Text>
+            <Text style={{ fontSize: 20 }}>{item.text}</Text>
+        </View>
+    )
+
+    if (initializing) {
+        getBBoardName()
+        setSelected()
+        setInitializing(false)
+    }
+
+    return (
+        <SafeAreaView style={[styles.container]}>
+            <View style={{ backgroundColor: "black", alignItems: "center", justifyContent: "center" }}>
+                <Text style={{ color: "red", fontSize: 42 }}>BBViewer</Text>
+            </View>
+            <View style={{ flexDirection: "row", height: 50 }}>
+                <Text
+                    style={{ backgroundColor: "blue", color: "white", fontSize: 16, textAlignVertical: "center", padding: 5 }}
+                    onPress={() => { getBBoardName() }}
+                >
+                    REFRESH BBOARDS
+                </Text>
+                <FlatList
+                    data={BBoardNames}
+                    renderItem={renderBBoardNames}
+                    keyExtractor={(item) => item}
+                    alignItems="flex-start"
+                    horizontal={true}
+                />
+            </View>
+            <View style={{ flexDirection: "row" }}>
+                <Text style={{ fontSize: 32 }}>Seleted bboard:</Text>
+                <Text style={{ fontSize: 32, backgroundColor: "black", color: "red" }}>
+                    {selected}
+                </Text>
+            </View>
+            <FlatList
+                data={BBoardContent}
+                renderItem={renderBBoardContent}
+                keyExtractor={(item) => item._id}
+                alignItems="flex-start"
+            />
+            <Text>DEBUGGING</Text>
+            <Text>bb:{selected}</Text>
+            <Text>show:false</Text>
+            <Text> bbs.length = {BBoardNames.length}</Text>
+            <Text>posts = {JSON.stringify(BBoardContent)}</Text>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  item: {
-    backgroundColor: 'lightgray',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-  debug: {
-    fontSize: 16,
-  },
-  toggle: {
-    fontSize: 24,
-    color: "blue" 
-  },
-  input: {
-    fontSize: 32
-  }
+    container: {
+        flex: 1,
+    },
+    bboardNameContainer: {
+        backgroundColor: "black",
+        padding: 10,
+        marginVertical: 8,
+        marginHorizontal: 8
+    },
+    BBoardNameText: {
+        fontSize: 16,
+        color: "red"
+    },
+    bboardContentContainer: {
+        backgroundColor: "lightgray",
+        padding: 20,
+        marginHorizontal: 8,
+        marginVertical: 0
+    }
 });
 
 export default App;
